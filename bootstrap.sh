a4fe0d37e22f6ec3ad0c346611b6de6f37a98757
Zois Pagoulatos <zpagoulatos@hotmail.com>
2024-10-23 09:29:36 +0200

    Update Homebrew installation instructions

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,7 +9,7 @@ fi
 
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 # Update Homebrew recipes
