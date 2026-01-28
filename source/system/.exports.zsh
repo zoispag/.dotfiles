8754e9bf3a5464961d71143e0e3d95f1af4233b1
Zois Pagoulatos <zpagoulatos@hotmail.com>
2026-01-28 10:49:58 +0100

    feat: add bun to path

--- source/system/.exports.zsh
+++ source/system/.exports.zsh
@@ -1,4 +1,4 @@
 export GOPATH=$(go env GOPATH)
 export GOROOT="$(brew --prefix golang)/libexec"
 export VOLTA_HOME="${HOME}/.volta"
-export PATH="~/scripts:${VOLTA_HOME}/bin:/usr/local/sbin:${HOME}/.composer/vendor/bin:${HOME}/.yarn/bin:${HOME}/.config/yarn/global/node_modules/.bin:${HOME}/.cargo/bin:${KREW_ROOT:-$HOME/.krew}/bin:${PATH}:${GOPATH}/bin:${GOROOT}/bin"
+export PATH="${HOME}/scripts:${VOLTA_HOME}/bin:/usr/local/sbin:${HOME}/.composer/vendor/bin:${HOME}/.yarn/bin:${HOME}/.bun/bin:${HOME}/.config/yarn/global/node_modules/.bin:${HOME}/.cargo/bin:${KREW_ROOT:-$HOME/.krew}/bin:${PATH}:${GOPATH}/bin:${GOROOT}/bin"
