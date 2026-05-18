8c259bf3bf78221d3f84e26456114edb2f72302f
Zois Pagoulatos <zpagoulatos@hotmail.com>
2026-05-18 10:47:03 +0200

    feat(opencode): Disable OMO telemetry

--- source/system/.exports.zsh
+++ source/system/.exports.zsh
@@ -2,3 +2,5 @@ export GOPATH=$(go env GOPATH)
 export GOROOT="$(brew --prefix golang)/libexec"
 export VOLTA_HOME="${HOME}/.volta"
 export PATH="${HOME}/scripts:${VOLTA_HOME}/bin:/usr/local/sbin:${HOME}/.composer/vendor/bin:${HOME}/.yarn/bin:${HOME}/.bun/bin:${HOME}/.config/yarn/global/node_modules/.bin:${HOME}/.cargo/bin:${KREW_ROOT:-$HOME/.krew}/bin:${PATH}:${GOPATH}/bin:${GOROOT}/bin"
+export OMO_SEND_ANONYMOUS_TELEMETRY=0
+export OMO_DISABLE_POSTHOG=1
